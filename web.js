10605eef053c667abd78077426929f6239cc053e
John Avgerinos <jag@aegean.gr>
2013-07-12 15:49:30 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
 var express = require('express');
-var buffer = new Buffer();
 var app = express.createServer(express.logger());
+
 var file = fs.readFileSync(index.html);
+var buffer = new Buffer(file);
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8',0,file));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
